106ae05b5ad5d65a8a5493e037fb131008f62239
Rayyan Tariq <raytariq93@gmail.com>
2022-09-16 10:23:17 +0500

    PWA complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CollectionViewComponent } from './collection-view/collection-view.compo
 import { DetailViewComponent } from './detail-view/detail-view.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,12 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     BrowserAnimationsModule,
     HammerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
